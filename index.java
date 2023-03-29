1535d07142dbf5532ece9da1452dc92aecf8361f
amiindeeq <amiindeeqxussen@gmail.com>
2023-03-29 08:32:32 +0300

    add .

---
+++ index.java
@@ -0,0 +1,31 @@
+package semester5;
+
+public class Bolean2 {
+	public static void main(String[] args) {
+		// this topics about if and else or true or false
+		int x = 20;
+		//String name = "Gued";
+		//double prince = 3.5;
+		
+		
+		if(x == 20) {
+			System.out.println("The x value is: "+x);  
+			//if its print if condition true
+		}
+		
+		else if(x == 100) {
+			
+		System.out.println("The x value is: "+ x);
+		}
+		
+		else if (x == 200) { 
+			// this condition true
+			System.out.println("The x value is: "+ x);
+		}
+		
+		else {
+			System.out.println("All the conditions are false");
+		}
+		
+	}
+	}
